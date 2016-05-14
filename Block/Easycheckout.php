e671a0aa600f9a36c9c06e3210fced747e9509e0
Sam Granger <sam@weprovide.com>
2016-05-14 14:32:14 +0200

    Add comments

--- Block/Easycheckout.php
+++ Block/Easycheckout.php
@@ -9,12 +9,18 @@ class Easycheckout extends Template
     protected $configProvider;
     protected $customerSession;
     protected $checkoutSession;
-    /**
-     * @var \Magento\Quote\Model\QuoteIdMaskFactory
-     */
     protected $quoteIdMaskFactory;
 
 
+    /**
+     * Easycheckout constructor.
+     * @param Template\Context $context
+     * @param array $data
+     * @param \Magento\Checkout\Model\CompositeConfigProvider $configProvider
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Magento\Checkout\Model\Session $checkoutSession
+     * @param \Magento\Quote\Model\QuoteIdMaskFactory $quoteIdMaskFactory
+     */
     public function __construct(
         Template\Context $context,
         array $data = [],
@@ -32,16 +38,25 @@ class Easycheckout extends Template
         $this->quoteIdMaskFactory = $quoteIdMaskFactory;
     }
 
+    /**
+     * @return array
+     */
     public function getConfig()
     {
         return $this->configProvider->getConfig();
     }
 
+    /**
+     * @return \Magento\Checkout\Model\Session
+     */
     public function getCheckoutSession()
     {
         return $this->checkoutSession;
     }
 
+    /**
+     * @return string
+     */
     public function getQuoteIdMask()
     {
         $quoteIdMask = $this->quoteIdMaskFactory->create()->load($this->getCheckoutSession()->getQuoteId(), 'quote_id')->getMaskedId();
--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -4,7 +4,12 @@ namespace Hackathon\Easycheckout\Controller\Index;
 class Index extends \Magento\Framework\App\Action\Action
 {
     protected $resultPageFactory;
-	
+
+    /**
+     * Index constructor.
+     * @param \Magento\Framework\App\Action\Context $context
+     * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+     */
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $resultPageFactory
@@ -13,6 +18,10 @@ class Index extends \Magento\Framework\App\Action\Action
         parent::__construct($context);
         $this->resultPageFactory = $resultPageFactory;
     }
+
+    /**
+     * @return \Magento\Framework\View\Result\Page
+     */
     public function execute()
     {
 		return $this->resultPageFactory->create();
