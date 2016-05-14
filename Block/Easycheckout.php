ee65bc66e8845da148e853d8240867cdfe3982e3
Sam Granger <sam@weprovide.com>
2016-05-14 12:44:57 +0200

    Make checkout config available in block

--- Block/Easycheckout.php
+++ Block/Easycheckout.php
@@ -6,11 +6,23 @@ use Magento\Framework\View\Element\Template;
 
 class Easycheckout extends Template
 {
+    protected $configProvider;
 
-    public function __construct(Template\Context $context, array $data = [])
+
+    public function __construct(
+        Template\Context $context,
+        array $data = [],
+        \Magento\Checkout\Model\CompositeConfigProvider $configProvider
+    )
     {
         parent::__construct($context, $data);
         $this->_isScopePrivate = true;
+        $this->configProvider = $configProvider;
+    }
+
+    public function getConfig()
+    {
+        return $this->configProvider->getConfig();
     }
 
 }
